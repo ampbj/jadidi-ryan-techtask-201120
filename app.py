1ebe3d5809da56ec35c78616d9d91b83e12815b3
Ryan Jadidi <rampbj@gmail.com>
2020-11-20 11:51:44 +1100

    Added Jinga2 template. lunch logic completed. Manual testing completed.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask
+from flask import Flask, render_template
 from src.lunch import LunchChoices
 
 app = Flask(__name__)
@@ -10,8 +10,8 @@ def suggest_lunch():
     with open('data/recipes.json') as recipes, open('data/ingredients.json') as ingredients:
         recipes = json.load(recipes)
         ingredients = json.load(ingredients)
-        lunchChoices = LunchChoices(recipes, ingredients)
-        return lunchChoices.choices()
+        lunchChoices = LunchChoices(recipes, ingredients).choices()
+        return render_template("base.html", data=lunchChoices)
 
 
 if __name__ == '__main__':
--- src/lunch.py
+++ src/lunch.py
@@ -9,13 +9,15 @@ class LunchChoices:
         self.today = date.today()
 
     def choices(self):
-        # seperating expired ingredients from still usable one by using following list comprehension.
-        usable_ingredients_dicts = [ingredient for ingredient in self.ingredients["ingredients"]
-                                    if datetime.strptime(ingredient["use-by"], '%Y-%m-%d').date() >= self.today]
-        usable_ingredients = [item['title']
-                              for item in usable_ingredients_dicts]
+        usable_ingredients = set()
+        best_before_expired_ingridiens = set()
+        for ingredient in self.ingredients["ingredients"]:
+            if datetime.strptime(ingredient["use-by"], '%Y-%m-%d').date() >= self.today:
+                usable_ingredients.add(ingredient['title'])
+                if datetime.strptime(ingredient["best-before"], '%Y-%m-%d').date() < self.today:
+                    best_before_expired_ingridiens.add(ingredient['title'])
         usable_recipes = [recipe for recipe in self.recipes["recipes"]
-                          if all(item in usable_ingredients for item in recipe["ingredients"])]
-        usable_ingredients_with_expired_best_before = [item['title'] for item in usable_ingredients_dicts
-                                                       if datetime.strptime(item["best-before"], '%Y-%m-%d').date() < self.today]
-        usable_recipes
+                          if set(recipe["ingredients"]).issubset(usable_ingredients)]
+        usable_recipes.sort(key=lambda recipe: best_before_expired_ingridiens.issubset(
+            set(recipe["ingredients"])))
+        return usable_recipes
